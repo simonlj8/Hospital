c2446b360bb17b7928c64ec0ffd6324a51a41b9e
Simon Ljungström <simonlj8@gmail.com>
2022-08-31 16:56:35 +0200

    First try

--- src/Patient.java
+++ src/Patient.java
@@ -1,4 +1,52 @@
+package edu.lernia.labb3;
+
+import java.lang.String;
+
+
 public class Patient {
+    boolean isSick = false;
+    String name;
+    String symptom;
+
+    String getSickness;
 
 
-}
+    Patient(String name,  String symptom) {
+        this.name = name;
+       // this.getSickness = getSickness;
+        this.symptom = symptom;
+        if (symptom != null) {
+            isSick();
+        }
+
+
+    }
+
+  public boolean isSick() {
+       return isSick = true ;
+   }
+
+
+    public Patient(String name) {
+        this.name = name;
+        isSick = false;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public String getSickness() {
+
+        return getSickness;
+    }
+
+    public void takeMedication (Medicine medicine){
+    if(getSickness != null && getSickness.equalsIgnoreCase(medicine.getTreatmentName())) {
+        isSick = false;
+        getSickness = null;
+    }
+        }
+    }
+
